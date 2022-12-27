33b8035601c0a45fcca65b9697aadbb9c9af8f3c
Simon Habchi <simon.habchi@utb.ecutbildning.se>
2022-12-27 23:11:43 +0100

    la till kod

--- tmdb-movie-app/src/components/Discover.js
+++ tmdb-movie-app/src/components/Discover.js
@@ -20,6 +20,43 @@ const Discover = () => {
     fetchAPI();
   }, []);
 
+  const handleGenreClick = async (genre_id) => {
+    setMovieByGenre(await fetchMovieByGenre(genre_id));
+  };
+
+  
+  const genreList = genres.map((item, index) => {
+      return (
+        <li className="list-inline-item" key={index}>
+          <button
+            type="button"
+            className="btn btn-outline-info"
+            onClick={() => {
+              handleGenreClick(item.id);
+            }}
+          >
+            {item.name}
+          </button>
+        </li>
+      );
+    });
+
+    // const movieList = movieByGenre.slice(0, 4).map((item, index) => {
+    //     return (
+    //       <div className="col-md-3 col-sm-6" key={index}>
+    //         <div className="card">
+    //           <Link to={`/movie/${item.id}`}>
+    //             <img className="img-fluid" src={item.poster} alt={item.title}></img>
+    //           </Link>
+    //         </div>
+    //         <div className="mt-3">
+    //           <p style={{ fontWeight: "bolder" }}>{item.title}</p>
+    //         </div>
+    //       </div>
+    //     );
+    //   });
+
+
   const discoverList = discover.slice(0, 4).map((item, index) => {
     return (
       <div className="col-md-3" key={index}>
@@ -36,6 +73,9 @@ const Discover = () => {
     );
   });
 
+
+
+
   return (
     <div className="container">
       <input type="text" placeholder='search'/>
@@ -47,14 +87,23 @@ const Discover = () => {
             Discover
           </p>
           <ul className style={{ color: "white" }} >
-        <li>dfsaas</li>
-        <li>dfsaas</li>
-        <li>dfsaas</li>
-        <li>dfsaas</li>
-        <li>dfsaas</li>
-        <li>dfsaas</li>
       </ul>
       </div>
+
+      <div className="row mt-3">
+        <div className="col">
+          <ul className="list-inline">{genreList}</ul>
+        </div>
+      </div>
+      
+      <div className="row mt-3">
+        <div className="col">
+          <ul className="list-inline">{movieByGenre}</ul>
+        </div>
+      </div>
+
+  
+
 
       <div className="row mt-3">
         <div className="col">
